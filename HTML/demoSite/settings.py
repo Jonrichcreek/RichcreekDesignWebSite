dc265cdf35b3c2ba51dd8774a365eed5ffb92f37
Jonrichcreek <jon.richcreek@gmail.com>
2019-07-03 20:15:57 -0400

    'RemoveEmail'

--- HTML/demoSite/settings.py
+++ HTML/demoSite/settings.py
@@ -142,13 +142,6 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
 
-#Email Config
-
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER='jon.richcreek@gmail.com'
-EMAIL_HOST_PASSWORD='6213385a'
-EMAIL_USE_TLS=True
 
 try:
     from.local_settings import *
