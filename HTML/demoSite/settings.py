97251e614f303abe7a737a5dd0a48a4e12de4223
Jonrichcreek <46690936+Jonrichcreek@users.noreply.github.com>
2021-01-20 18:06:15 -0500

    Initial

--- HTML/demoSite/settings.py
+++ HTML/demoSite/settings.py
@@ -142,6 +142,13 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
 
+#Email Config
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER='jon.richcreek@gmail.com'
+EMAIL_HOST_PASSWORD='6213385a'
+EMAIL_USE_TLS=True
 
 try:
     from.local_settings import *
